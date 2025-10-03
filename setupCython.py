c312c335034d7d388d0c5f0d30ae1f9b672df9dd
Rorry Brenner <47458848+RorryB@users.noreply.github.com>
2025-10-02 21:48:09 -0400

    updated pep8 comments
    * updated setups
    
    * Add package wheels for b2.0.2.1 (Python 3.9)
    
    * Add package wheels for b2.0.2.1 (Python 3.8)
    
    * Add package wheels for b2.0.2.1 (Python 3.11)
    
    * Add package wheels for b2.0.2.1 (Python 3.10.15)
    
    * Add package wheels for b2.0.2.1 (Python 3.6)
    
    * Add package wheels for b2.0.2.1 (Python 3.12)
    
    * Add package wheels for b2.0.2.1 (Python 3.7)
    
    * Add package manylinux wheels for b2.0.2.1
    
    * Add package wheels for b2.0.2.1 (Python 3.12)
    
    * Add package wheels for b2.0.2.1 (Python 3.13)
    
    * Add package wheels for b2.0.2.1 (Python 3.9)
    
    * Add package wheels for b2.0.2.1 (Python 3.11)
    
    * Add package wheels for b2.0.2.1 (Python 3.8)
    
    * Add package wheels for b2.0.2.1 (Python 3.10.x)
    
    * Add package wheels for b2.0.2.1 (Python 3.11)
    
    * Add package wheels for b2.0.2.1 (Python 3.9)
    
    * Add package wheels for b2.0.2.1 (Python 3.10.x)
    
    * Add package wheels for b2.0.2.1 (Python 3.12)
    
    * Add package wheels for b2.0.2.1 (Python 3.6)
    
    * Add package wheels for b2.0.2.1 (Python 3.8)
    
    * Add package wheels for b2.0.2.1 (Python 3.7)
    
    * Add package wheels for b2.0.2.1 (Python 3.8)
    
    * Add package wheels for b2.0.2.1 (Python 3.9)
    
    * Add package wheels for b2.0.2.1 (Python 3.10.15)
    
    * Add package wheels for b2.0.2.1 (Python 3.11)
    
    * Add package wheels for b2.0.2.1 (Python 3.12)
    
    * Add package wheels for b2.0.2.1 (Python 3.6)
    
    * Add package wheels for b2.0.2.1 (Python 3.7)
    
    * finalized comments and updated readmes
    
    * cleaned up
    
    ---------
    
    Co-authored-by: PAI GitHub Actions <actions@perforatedai.dev.local>

--- setupCython.py
+++ setupCython.py
@@ -53,7 +53,7 @@ class build_py(_build_py):
 
 setup(
     name="perforatedai",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     author="PerforatedAI",
     author_email="rorry@perforatedai.com",
