985f5df127fd5b6d136ab6f990a9579d862ac218
Yuvraj Singh <71546888+YuvrajSHAD@users.noreply.github.com>
2026-01-17 17:47:45 +0530

    Update 4_sweep_perforated.py

--- Examples/hackathonProjects/edge-voice-classification/4_sweep_perforated.py
+++ Examples/hackathonProjects/edge-voice-classification/4_sweep_perforated.py
@@ -22,9 +22,8 @@ warnings.filterwarnings("ignore")
 from perforatedai import globals_perforatedai as GPA
 from perforatedai import utils_perforatedai as UPA
 
-os.environ["PAIEMAIL"] = "hacker@perforatedai.com"
-os.environ["PAITOKEN"] = "InJ9BjZSB+B+l30bmSzhqOwsXxOx0NRKAe8dtdAqdQcT/pKjmme1fqB1zrnCd5CWNrhJm40PVjaDbIrjR5xU+q2uhcUWX8gk2Kb2lHjafkUnizPXyP+yckbv+UxlU25ZlrvC3XlLu/AZdVKJE7Eov9+4c76sKe2hbRnH1fny2xIPYmy2/m/sY1gxXbhPtTa1mtxk2EgLeo5pRu/eL/7pSXWmEoRmvVorgQEJzt1VYOZyp0vP4bLxF72tOgSjXGBO8SHHcN16CbOVJuIEm3jmEc/AfPyyB+G4TEqhH7UZ0W2R/bnXtNberKqF2bQTuyT26etQw6NEMoXwuugDcrBXEw=="
-
+os.environ["PAIEMAIL"] = "EMAIL"
+os.environ["PAITOKEN"] = "TOKEN"
 # Dataset classes
 class SubsetSC(SPEECHCOMMANDS):
     def __init__(self, subset: str = None):
@@ -367,3 +366,4 @@ def train_sweep():
 
 if __name__ == '__main__':
     train_sweep()
+
